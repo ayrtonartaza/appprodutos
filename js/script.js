e12717100c186e98eaebebfb80498ba480250d8b
adrian <adriancdma@gmal.com>
2019-06-21 19:01:25 -0300

    se pueden escribir los datos y quedan guardados por el localstorage

--- js/script.js
+++ js/script.js
@@ -27,20 +27,35 @@ document.getElementById("formulario").addEventListener("submit",guardarDatos);
  
    
    /* escribir loe elmentos introducidos en el input */
-   document.getElementById("vista").innerHTML +=`
    
-   <li id="items"> ${nombre} ${telefono} ${producto} ${precio} 
-   <input type="button"   onclick="borrarDatos(nombre)" value="eliminar" class="botoneliminar btn-flat red">
-   </li>  `;
  
    document.getElementById("formulario").reset();
   e.preventDefault(); 
   
-  
+  escribirDatos();
    
    
  };
 
+ function escribirDatos(){
+   let datos = JSON.parse(localStorage.getItem("datos"));
+   for(let i =0; i<datos.length; i++){
+     let nombre = datos[i].nombre
+     let telefono = datos[i].telefono;
+     let producto = datos[i].producto;
+     let precio = datos[i].precio;
+     
+
+
+     document.getElementById("vista").innerHTML +=`
+   
+  <li id="items"> ${nombre} ${telefono} ${producto} ${precio} 
+  <input type="button"   onclick="borrarDatos(nombre)" value="eliminar" class="botoneliminar btn-flat red">
+  </li>  `;
+   }
+   
+  
+ }
 
  /* borar li con boton borrar */
  function borrarDatos (){
@@ -50,7 +65,7 @@ document.getElementById("formulario").addEventListener("submit",guardarDatos);
   
  }
 
-
+escribirDatos();
   
  
 
